59beaa5df0100c5df923985bfe65a0c638eaef04
Wendell <wendellcs131@gmail.com>
2024-03-16 23:03:17 -0300

    Starting repos page

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
+import { Container } from "./styles";
 
 function Repositorio() {
     const { repositorio } = useParams()
+
+    const [repo, setRepo] = useState({})
+    const [issues, setIssues] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function load() {
+            const nomeRepo = decodeURIComponent(repositorio)
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ])
+
+            setRepo(repositorioData.data)
+            setIssues(issuesData.data)
+            setLoading(false)
+        }
+
+        load()
+
+    }, [repositorio])
+
     return (
-        <div style={{ color: 'white' }}>
-            {decodeURIComponent(repositorio)}
-        </div>
+        <Container>
+
+        </Container>
     )
 }
 
