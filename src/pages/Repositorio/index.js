49e16f17174ac74fbd57fd3a692f00f17a65ea67
Wendell <wendellcs131@gmail.com>
2024-03-21 00:22:38 -0300

    Add repo HTML and CSS

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
 
-import { Container } from "./styles";
+import { Container, Owner, Loading, BackButton } from "./styles";
+import { FaArrowLeft } from 'react-icons/fa';
 
 function Repositorio() {
-    const { repositorio } = useParams()
+    const { repositorio } = useParams();
 
-    const [repo, setRepo] = useState({})
-    const [issues, setIssues] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [repo, setRepo] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function load() {
-            const nomeRepo = decodeURIComponent(repositorio)
+            const nomeRepo = decodeURIComponent(repositorio);
 
             const [repositorioData, issuesData] = await Promise.all([
                 api.get(`/repos/${nomeRepo}`),
@@ -23,22 +24,39 @@ function Repositorio() {
                         per_page: 5
                     }
                 })
-            ])
+            ]);
 
-            setRepo(repositorioData.data)
-            setIssues(issuesData.data)
-            setLoading(false)
+            setRepo(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
         }
 
         load()
 
-    }, [repositorio])
+    }, [repositorio]);
+
+
+    if (loading) {
+        return (
+            <Loading>
+                Carregando...
+            </Loading>
+        )
+    }
 
     return (
         <Container>
+            <BackButton to={'/'}><FaArrowLeft color="#000" size={35} /></BackButton>
+            <Owner>
+                <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+
+                <h1>{repo.name}</h1>
+
+                <p>{repo.description}</p>
+            </Owner>
 
         </Container>
     )
-}
+};
 
-export default Repositorio
+export default Repositorio;
